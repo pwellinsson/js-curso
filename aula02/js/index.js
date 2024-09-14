f1f95b5f8b4f50a3f67ca0a60ee7c446a0ef187d
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-09-14 18:34:05 -0300

    Completed aula02

--- aula02/js/index.js
+++ aula02/js/index.js
@@ -1,1 +1,4 @@
-alert('Um alerta rodando de uma página externa!');
+document.getElementById("Texto").innerHTML="Meu primeiro texto <strong>JS</strong>";
+document.getElementById("Soma").innerHTML=1 +4;
+alert(20 + 2);
+console.log("Oi, isso é um console.log")
