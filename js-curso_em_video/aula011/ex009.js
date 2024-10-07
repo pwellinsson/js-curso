eedef4e91851242e8ba39c435537c232a927f5e7
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-10-07 16:09:01 -0300

    Completed aula011 of CEV

--- js-curso_em_video/aula011/ex009.js
+++ js-curso_em_video/aula011/ex009.js
@@ -1,7 +1,15 @@
-pais = 'França'
-console.log(`Vivendo em ${pais}`)
-if(pais == 'Brasil') {
-    console.log('Você é Brasileiro!')
-} else {
-    console.log('Você é Estrangeiro')
+function nascimento() {
+    pais = document.querySelector('input#nasc').value
+    res = document.querySelector('div#resp')
+    res.innerHTML = `<p>Vivendo em ${pais}</p>`
+    if(pais == 'Brasil') {
+        res.innerHTML += '<p>Você é Brasileiro!</p>'
+    } else {
+        res.innerHTML += '<p>Você é Estrangeiro</p>'
+    }
+}
+
+function remover() {
+    res = document.querySelector('div#resp')
+    res.innerText = ''
 }
