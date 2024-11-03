8c756275947a529aa57e01ecb0f39d87f90f68b0
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-11-03 18:29:27 -0300

    Finalize incompleted of ex014

--- js-curso_em_video/aula12ex/ex014/script.js
+++ js-curso_em_video/aula12ex/ex014/script.js
@@ -1,18 +1,21 @@
 function carregar() {
     msg = document.querySelector('div#msg')
-    img = document.querySelector('img#image')
+    img = document.querySelector('div#image')
     data = new Date()
     // hora = data.getHours()
-    hora = 21
+    hora = 12
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         // Good Morning
         img.src = "img/foto_manha.png"
-    } else if (hora >= 12 && hora < 18) {
+        document.body.style.background = '#d6b087'
+    } else if (hora >= 12 && hora <= 18) {
         // Good Afternom
         img.src = "img/foto_tarde.png"
+        document.body.style.background = '#ee7002'
     } else {
         // Good Night
         img.src = "img/foto_noite.png"
+        document.body.style.background = '#3d80b4'
     }
 }
