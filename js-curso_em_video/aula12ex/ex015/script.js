9b724670a140c86d7eb036c09a3d4f647abf77d9
P-Wellinsson <pedrowellinsson368@gmail.com>
2024-11-05 08:34:23 -0300

    Insert a part of JS

--- js-curso_em_video/aula12ex/ex015/script.js
+++ js-curso_em_video/aula12ex/ex015/script.js
@@ -1,4 +1,7 @@
-function Verificar() {
+function verificar() {
+    data = New.Date
+    ano = data.getFullYear()
+    alert(`data: ${data} and ano: ${ano}`)
     idade = document.querySelector('div#nascimento')
     resultado = document.getElementById('res')
     resultado.innerHTML = 'oie'
