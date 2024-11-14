628e4bfe3ee667d00c2e7541ea5dce34404f7cc0
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-11-14 16:20:51 -0300

    Insert a imcompleted code

--- js-curso_em_video/aula12ex/ex015/script.js
+++ js-curso_em_video/aula12ex/ex015/script.js
@@ -1,8 +1,14 @@
 function verificar() {
-    data = New.Date
+    data = new Date()
     ano = data.getFullYear()
-    alert(`data: ${data} and ano: ${ano}`)
-    idade = document.querySelector('div#nascimento')
-    resultado = document.getElementById('res')
-    resultado.innerHTML = 'oie'
+    nasc = document.querySelector('div#nasc')
+    res = document.getElementById('res')
+    if (Number(nasc.value.length) > ano) {
+        alert('Verifique os dados e tente novamente')
+    } else {
+        alert('Tudo certo até agora')
+    }
+    /*age = ano - nasc
+    alert(`ano: ${ano}, idade ${age} e nascimento ${nasc.value}`)
+    res.innerHTML = 'oie'*/
 }
