831ae9674890b264c15f1ef924dbb99bc2eb6d82
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-11-14 21:22:04 -0300

    Insert a some modifications of 2class

--- js-programacao_web/aula02/js/index.js
+++ js-programacao_web/aula02/js/index.js
@@ -1,4 +1,4 @@
 document.getElementById("Texto").innerHTML="Meu primeiro texto <strong>JS</strong>! ";
-document.getElementById("Soma").innerHTML=1 +4;
-alert(20 + 2);
+document.getElementById("Soma").innerHTML= 1 + 4;
+// alert(20 + 2);
 console.log("Hi, this is a console.log");
