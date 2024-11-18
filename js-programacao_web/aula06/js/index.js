6c7efe3e8ba3a1621820b0f540ecf07c9717784b
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2024-11-18 19:45:17 -0300

    Finalize aula06 of P.W.

--- js-programacao_web/aula06/js/index.js
+++ js-programacao_web/aula06/js/index.js
@@ -4,7 +4,27 @@
 -> const
 */
 
-var a = 1;
-var b = 10;
-var c = a + b;
-alert(c)
+/*
+// Declaração de Variáveis
+var nome, sobrenome, nome_completo, idade;
+
+// Atribuição dos Valores
+
+nome = "Dimitri";
+sobrenome = "Teixeira";
+idade = 30;
+nome_completo = nome + " " + sobrenome;
+
+pessoa = idade + nome;
+
+document.querySelector("p#Texto").innerHTML = nome_completo;
+// console.log(nome_completo);
+*/
+
+let x = 100;
+
+{
+    let x = 12;
+}
+
+document.querySelector("p#Texto").innerHTML = x;
