59b16fc4f769a98dcc7ceb7afa5123637aee164f
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2024-11-20 21:14:32 -0300

    Finalize P.W.\aula08

---
+++ js-programacao_web/aula08/js/index.js
@@ -0,0 +1,5 @@
+function soma(valor1 = 20, valor2 = 1) {
+    total = valor1 + valor2;
+  document.querySelector("p#texto").innerHTML = total;
+}
+// document.querySelector("p#soma").innerHTML = soma((valor2 = 5), (valor1 = 10));
