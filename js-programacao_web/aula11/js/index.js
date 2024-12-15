b81c957134a5e8effe11742decd5a81545ecdd44
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2024-12-14 21:02:13 -0300

    Finalize PW\aula11 of Array

---
+++ js-programacao_web/aula11/js/index.js
@@ -0,0 +1,42 @@
+/*
+  Arrays/Matrizes são como listas em Python.
+  Onde dentro de um Array pode ter tanto outros Arrays, como também funções.
+  Também existem objetos, Que são muitos parecidos com os dicionários em python.
+*/
+
+/*
+
+let list = ["Batata", "Cenoura", "Beterraba", "Maça"];
+// let list = new Array("Batata", "Cenoura", "Beterraba", "Maça");
+let list2 = [];
+// Mudando valor de uma lista ou adicionando valor à uma lista/Array.
+list2[0] = "abacate";
+list2[1] = "Jabuticaba";
+list2[2] = "Berinjela";
+list2[3] = "Alcassuz";
+
+list[1] = "Couve-Flor";
+
+console.log(list[1]); // Mostrar 1 Valor da lista.
+console.log(list); // Mostrar a quantidade de itens no Array e todos os itens da lista.
+
+*/
+
+/*
+const pessoa = ["Dimitri", "Texeira", 30, "Professor"]; // Array (Lista)
+
+console.log(pessoa.length) //Mostra os itens em um Array
+
+console.log(pessoa[pessoa.length - 1]) // Mostra o ultimo item da lista.
+pessoa.push("Brasileiro") // Insere um item no final da lista.
+
+console.log(pessoa)
+
+const pessoa1 = { nome: "Dimitri", sobrenome: "Teixeira", idade: 30 }; // Objeto(Dicionário)
+
+pessoa1.nome;
+console.log(pessoa[0]);
+console.log(pessoa1.sobrenome)
+
+alert(Array.isArray(pessoa)) // Informa se "pessoa" é Array == true
+*/
