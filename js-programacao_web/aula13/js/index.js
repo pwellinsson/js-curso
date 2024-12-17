7731c580e32d5def291d09e8bf3c4b1d34944fe3
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2024-12-17 19:41:12 -0300

    Finalize PW\aula13

--- js-programacao_web/aula13/js/index.js
+++ js-programacao_web/aula13/js/index.js
@@ -1,1 +1,26 @@
 /* if else */
+/*
+var hora = new Date().getHours();
+
+if (hora < 12) {
+  alert("Bom dia");
+} else if (hora < 18) {
+  alert("Boa Tarde");
+} else {
+  alert("Boa Noite");
+}
+*/
+
+function verificar() {
+  let nome = document.querySelector("input#nome").value;
+
+  if (nome == "" || nome == null) {
+    let p = document.querySelector("p#texto");
+    p.innerHTML = "O campo não pode ser vazio";
+    p.style.color = "red";
+  } else {
+    let p = document.querySelector("p#texto");
+    p.innerHTML = "Parabéns <br> Tudo Certo";
+    p.style.color = "blue";
+  }
+}
