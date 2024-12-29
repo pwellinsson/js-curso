5dccd10a80cb8041939830883745bcad0eea9852
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2024-12-29 14:14:02 -0300

    Insert a part of PW\aula15

--- js-programacao_web/aula15/js/index.js
+++ js-programacao_web/aula15/js/index.js
@@ -2,3 +2,19 @@
 Laços oferecem uma maneira de executar um bloco de código várias vezes.
 
 */
+
+function InsereNum() {
+  console.log("Início do laço, Safe!");
+  //  (Valor inicial; Condição; Execução)
+  for (let i = 10; i < 100001; i++) {
+    if (i > 10004 || i < 0) {
+      console.log("Fim do laço, Safe!");
+    }
+
+    document.querySelector("p#texto").innerHTML += `, ${i}`;
+  }
+}
+
+function TiraNum() {
+  document.querySelector("p#texto").innerHTML = "1, 2, 3, 4, 5, 6, 7, 8, 9";
+}
