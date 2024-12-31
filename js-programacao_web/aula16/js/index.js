a06460c16d5be58ac565a8bd198c56e520cdc372
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2024-12-31 11:46:33 -0300

    Insert a basic code of PW\aula16

---
+++ js-programacao_web/aula16/js/index.js
@@ -0,0 +1,14 @@
+/* EVENTOS DE TEMPO COM JAVASCRIPT
+
+Os eventos de tempo permitem à exexução do código em intervalos de tempo específicados. Esses intervalos de tempo são chamados de eventos de cronômetro.
+
+Os dois métodos-chaves para usar com JS são:
+
+setTimeout(function, tempo em milisegundos)
+-> Executa uma função, após esperar um número específico de milisegundos.
+
+setInterval(function, milliseconds)
+-> Executa uma função, repetidamente, em intervalos de tempo específicos.
+
+*/
+ 
