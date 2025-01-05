60fcdc2adedb91ac5b5548965f251aadc8584560
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2025-01-05 10:48:56 -0300

    Finalize PW\aula17

--- js-programacao_web/aula17/js/index.js
+++ js-programacao_web/aula17/js/index.js
@@ -1,6 +1,7 @@
-/** CLASSES EM JS
+/** CLASSES EM JS:
 
- */
+ * Parecido com classes em Python.
+*/
 
 class Carro {
   constructor(marca, modelo, ano, data_cadastro = new Date().getFullYear()) {
@@ -9,9 +10,20 @@ class Carro {
     this.ano = ano;
     this.data_cadastro = data_cadastro;
   }
+
+  buzinar() {
+    return `${this.modelo} buzinou!`;
+  }
 }
 
 const uno = new Carro("Fiat", "Uno", 2002);
-const lanborguini = new Carro("Lamborguini", "", 2020);
+const lanborguini = new Carro("Strogonnof", "Lamborguini", 2101, 2019);
 
 console.log(uno);
+console.log(lanborguini);
+console.log("Data de Cadastro: " + uno.data_cadastro);
+
+console.log(lanborguini.buzinar());
+
+lanborguini.ano = 2002;
+console.log("Lanborguini atualizado: ", lanborguini);
