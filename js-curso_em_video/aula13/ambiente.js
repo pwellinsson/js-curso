93460972586cd557b2b7edefe5e3a1ed14501854
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2025-02-28 17:06:23 -0300

    Finalize ambiente.js

---
+++ js-curso_em_video/aula13/ambiente.js
@@ -0,0 +1,24 @@
+/*
+let c = 1;
+do {
+  console.log(`Teste ${c}`);
+  c++;
+} while (c < 6);
+*/
+
+/*
+let cont = 1;
+while (cont <= 500) {
+  console.log("Tudo bem? n°", cont);
+  cont++;
+}
+*/
+
+/*
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+*/
