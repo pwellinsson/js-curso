43f1849628e0b212f23b638cc2e9790852385c28
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2025-03-01 21:24:18 -0300

    Create ambiente.js

---
+++ js-curso_em_video/aula14/ambiente.js
@@ -0,0 +1,14 @@
+console.log("Vai começar");
+
+for (var c = 1; c <= 10; c++) {
+  console.log(`Teste N° ${c}`);
+}
+
+console.log("Fim");
+
+/*
+console.log("Pausa");
+for (var c = -1; c >= -10; c--) {
+  console.log(`Teste N° ${c}`);
+}
+*/
